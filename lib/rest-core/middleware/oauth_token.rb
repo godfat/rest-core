163a1936eeec3b25aee651f231335c4a5023aa56
Lin Jen-Shin <godfat@godfat.org>
2011-05-31 16:05:42 +0800

    oauth_token.rb: oauth_token_key -> oauth_token_name

--- lib/rest-core/middleware/oauth_token.rb
+++ lib/rest-core/middleware/oauth_token.rb
@@ -2,11 +2,11 @@
 require 'rest-core/middleware'
 
 class RestCore::OauthToken
-  def self.members; [:oauth_token_key, :oauth_token]; end
+  def self.members; [:oauth_token_name, :oauth_token]; end
   include RestCore::Middleware
 
   def call env
     app.call(env.merge(REQUEST_QUERY => env[REQUEST_QUERY].merge(
-      oauth_token_key(env) => oauth_token(env))))
+      oauth_token_name(env) => oauth_token(env))))
   end
 end
