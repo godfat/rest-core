70dc0af3cd703817d0faaa4e439b335aadb81fbd
Lin Jen-Shin <godfat@godfat.org>
2011-06-14 22:19:23 +0800

    test_timeout.rb: friendlier test

--- test/test_timeout.rb
+++ test/test_timeout.rb
@@ -7,15 +7,10 @@ end
 
 describe RestGraph do
   should 'respect timeout' do
-    stub_request(:get, 'https://graph.facebook.com/me').to_return(
-      lambda{ |r| sleep(0.05); '{}' })
-    e = nil
-    begin
-      RestGraph.new(:timeout => 0.01).get('me')
-      nil.should == 'timeout must be thrown'
-    rescue Timeout::Error => e
-    end
-    e.should.kind_of?(Timeout::Error)
+    stub_request(:get, 'https://graph.facebook.com/me').
+      to_return(:body => '{}')
+    mock.proxy(Timeout).timeout(numeric)
+    RestGraph.new.get('me').should == {}
   end
 
   should 'override timeout' do
