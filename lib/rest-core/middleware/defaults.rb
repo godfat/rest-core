7f6dba146f563fe8f708c6ecf257265604083513
Lin Jen-Shin <godfat@godfat.org>
2011-06-14 21:47:38 +0800

    defaults.rb: since singleton_class make serialize hard.. :( change to:
    method_missing and respond_to? approach. i don't like this, but that's
    only we have.

--- lib/rest-core/middleware/defaults.rb
+++ lib/rest-core/middleware/defaults.rb
@@ -5,18 +5,40 @@ class RestCore::Defaults
   def self.members; [:defaults]; end
   include RestCore::Middleware
 
-  def initialize app, defaults
-    super
-    singleton_class.module_eval do
-      defaults.each{ |(key, value)|
-        define_method(key) do |env|
-          if value.respond_to?(:call)
-            value.call
-          else
-            value
-          end
-        end
-      }
+  # the use of singleton_class is making serialization hard!
+  # def initialize app, defaults
+  #   super
+  #   singleton_class.module_eval do
+  #     defaults.each{ |(key, value)|
+  #       define_method(key) do |env|
+  #         if value.respond_to?(:call)
+  #           value.call
+  #         else
+  #           value
+  #         end
+  #       end
+  #     }
+  #   end
+  # end
+
+  def method_missing msg, *args, &block
+    env = args.first
+    if env.kind_of?(Hash) && (d = defaults(env)) && d.key?(msg)
+      if (value = defaults(env)[msg]).respond_to?(:call)
+        value.call
+      else
+        value
+      end
+    else
+      super
+    end
+  end
+
+  def respond_to? msg
+    if (d = defaults({})) && d.key?(msg)
+      true
+    else
+      super
     end
   end
 end
