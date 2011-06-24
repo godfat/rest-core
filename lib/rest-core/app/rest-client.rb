631959d580413bd67087cd9c52508859a0ee08c0
Lin Jen-Shin <godfat@godfat.org>
2011-06-25 03:14:57 +0800

    add RESPONSE_STATUS

--- lib/rest-core/app/rest-client.rb
+++ lib/rest-core/app/rest-client.rb
@@ -6,16 +6,19 @@ require 'restclient'
 class RestCore::RestClient
   include RestCore::Middleware
   def call env
-    response =
-      ::RestClient::Request.execute(:method  => env[REQUEST_METHOD ],
-                                    :url     => request_uri(env)    ,
-                                    :payload => env[REQUEST_PAYLOAD],
-                                    :headers => env[REQUEST_HEADERS])
+    respond(env,
+            ::RestClient::Request.execute(:method  => env[REQUEST_METHOD ],
+                                          :url     => request_uri(env)    ,
+                                          :payload => env[REQUEST_PAYLOAD],
+                                          :headers => env[REQUEST_HEADERS]))
 
-    env.merge(RESPONSE_BODY    => response.body,
-              RESPONSE_HEADERS => response.raw_headers)
   rescue ::RestClient::Exception => e
-    env.merge(RESPONSE_BODY    => e.response.body,
-              RESPONSE_HEADERS => e.response.raw_headers)
+    respond(env, e.response)
+  end
+
+  def respond env, response
+    env.merge(RESPONSE_BODY    => response.body,
+              RESPONSE_STATUS  => response.code,
+              RESPONSE_HEADERS => response.raw_headers)
   end
 end
