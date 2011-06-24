13ae959a52b5454d977b3efa2359b5a422903825
Lin Jen-Shin <godfat@godfat.org>
2011-06-24 23:22:29 +0800

    so, we have to-be-refactored twitter client now

---
+++ lib/rest-core/client/twitter.rb
@@ -0,0 +1,51 @@
+
+RestCore::Builder.client('Twitter', :data) do
+  s = self.class # this is only for ruby 1.8!
+  use s::Timeout       , 10
+
+  use s::DefaultSite   , 'https://api.twitter.com/'
+  use s::DefaultHeaders, {'Accept' => 'application/json'}
+  use s::DefaultQuery  , {'format' => 'json'}
+  use s::Oauth1Header  ,
+    'oauth/request_token', 'oauth/access_token', 'oauth/authorize',
+    nil, nil, nil, nil, nil, nil
+
+  use s::CommonLogger  , method(:puts)
+
+  use s::Cache         , {}, nil do
+    use s::ErrorHandler  , lambda{|env| raise env[s::RESPONSE_BODY]['error'] }
+    use s::ErrorDetector , lambda{|env| env[s::RESPONSE_HEADERS]['status'].
+                                        first !~ /^2/}
+    use s::JsonDecode    , true
+    run s::Ask
+  end
+
+  use s::Defaults      , :data     => lambda{{}}
+
+  run s::RestClient
+end
+
+module Twitter::Client
+  include RestCore
+
+  def oauth_token
+    data['oauth_token'] if data.kind_of?(Hash)
+  end
+  def oauth_token=  token
+    data['oauth_token'] = token if data.kind_of?(Hash)
+  end
+  def oauth_token_secret
+    data['oauth_token_secret'] if data.kind_of?(Hash)
+  end
+  def oauth_token_secret=  secret
+    data['oauth_token_secret'] = secret if data.kind_of?(Hash)
+  end
+
+  private
+  def set_token query
+    self.data = query
+  end
+end
+
+Twitter.send(:include, RestCore::ClientOauth1)
+Twitter.send(:include,  Twitter::Client)
---
+++ lib/rest-core/client_oauth1.rb
@@ -0,0 +1,25 @@
+
+require 'rack'
+
+module RestCore::ClientOauth1
+  include RestCore
+
+  def authorize_url!
+    set_token(Rack::Utils.parse_query(
+      post(request_token_path, {}, {}, {:json_decode => false})))
+
+    url(authorize_path, :oauth_token => oauth_token, :format => false)
+  end
+
+  def authorize! verifier
+    set_token(Rack::Utils.parse_query(
+      post(access_token_path, {}, {}, {:verifier => verifier,
+                                       :json_decode => false})))
+  end
+
+  private
+  def set_token query
+    self.oauth_token        = query['oauth_token']
+    self.oauth_token_secret = query['oauth_token_secret']
+  end
+end
