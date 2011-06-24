08696db0f95454c7fa206568cdf1f2d1d9a61fa0
Lin Jen-Shin <godfat@godfat.org>
2011-06-24 18:11:03 +0800

    test_oauth1_header.rb: test for signature

--- test/test_oauth1_header.rb
+++ test/test_oauth1_header.rb
@@ -22,7 +22,9 @@ describe RestCore::Oauth1Header do
        'oauth_version'      => '1.0'                                        ,
        'oauth_signature_method' => 'HMAC-SHA1'}
 
-    @auth = RestCore::Oauth1Header.new(RestCore::Ask.new, nil, nil, nil)
+    @auth = RestCore::Oauth1Header.new(RestCore::Ask.new,
+      'GDdmIQH6jhtmLUypg82g',
+      'MCD8BKwGdgPHvAuvgvz4EQpqDAtx89grbuNMRd7Eh98', nil)
   end
 
   should 'have correct base_string' do
@@ -34,5 +36,10 @@ describe RestCore::Oauth1Header do
       'e%3DQP70eNmVz8jvdPevU3oJD2AfF7R7odC2XJcn4XlZJqk%26oauth_sig' \
       'nature_method%3DHMAC-SHA1%26oauth_timestamp%3D1272323042%26' \
       'oauth_version%3D1.0')
+  end
+
+  should 'have correct signature' do
+    @auth.signature(@env, @oauth_params).should.equal(
+      '8wUi7m5HFQy76nowoCThusfgB+Q=')
   end
 end
