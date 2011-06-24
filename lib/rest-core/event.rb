868f7eaaf3dd6f1f9a8a694f66cbcf62d7db4651
Lin Jen-Shin <godfat@godfat.org>
2011-06-24 17:55:06 +0800

    event.rb: change event struct from (duration, url) to (duration, message)

--- lib/rest-core/event.rb
+++ lib/rest-core/event.rb
@@ -1,12 +1,12 @@
 
 module RestCore
-  EventStruct = Struct.new(:duration, :url) unless
+  EventStruct = Struct.new(:duration, :message) unless
     RestCore.const_defined?(:EventStruct)
 
   class Event < EventStruct
     # self.class.name[/(?<=::)\w+$/] if RUBY_VERSION >= '1.9.2'
-    def name; self.class.name[/::\w+$/].tr(':', ''); end
-    def to_s; "spent #{sprintf('%f', duration)} #{name} #{url}";end
+    def name; self.class.name[/::(\w+)$/, 1]        ; end
+    def to_s; "spent #{duration} #{name} #{message}"; end
   end
   class Event::MultiDone    < Event; end
   class Event::Requested    < Event; end
