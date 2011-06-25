63a8c30ac45f7043810b163abe628513614dd6fa
Lin Jen-Shin <godfat@godfat.org>
2011-06-26 00:51:50 +0800

    twitter.rb: twitter didn't like foramt=json

--- lib/rest-core/client/twitter.rb
+++ lib/rest-core/client/twitter.rb
@@ -5,7 +5,6 @@ RestCore::Builder.client('Twitter', :data) do
 
   use s::DefaultSite   , 'https://api.twitter.com/'
   use s::DefaultHeaders, {'Accept' => 'application/json'}
-  use s::DefaultQuery  , {'format' => 'json'}
   use s::Oauth1Header  ,
     'oauth/request_token', 'oauth/access_token', 'oauth/authorize',
     nil, nil, nil, nil, nil, nil
