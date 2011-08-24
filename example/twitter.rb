f7230a17817e2b53fc801670e23200e905517f34
Lin Jen-Shin <godfat@godfat.org>
2011-08-24 22:03:06 +0800

    fix example/twitter.rb

--- example/twitter.rb
+++ example/twitter.rb
@@ -8,5 +8,4 @@ twitter.authorize_url!   # copy and paste the URL in browser to authorize
 twitter.authorize!('..') # paste your code from browser
 p twitter.tweet('hi!')   # tweet for the current user
 
-p twitter.post('statuses/update_with_media.json',
-  :status => 'hi!', 'media[]' => File.open('...'))
+p twitter.tweet('hi with pic!', File.open('...'))
