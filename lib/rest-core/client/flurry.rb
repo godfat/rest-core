24631e47b3b22f6c683a798544cfd8b4face021c
Lin Jen-Shin <godfat@godfat.org>
2011-09-19 22:02:02 +0800

    flurry.rb: fix algorithm and update example

--- lib/rest-core/client/flurry.rb
+++ lib/rest-core/client/flurry.rb
@@ -43,9 +43,16 @@ module RestCore::Flurry::Client
 
   # see: http://wiki.flurry.com/index.php?title=AppMetrics
   # >> f.metrics('ActiveUsers', {}, :weeks => 4)
-  # => [["2011-09-16", 7929], ["2011-09-15", 36453], ["2011-09-14", 34026],
-  #     ["2011-09-13", 34245], ["2011-09-12", 36879], ["2011-09-11", 44101],
-  #     ["2011-09-10", 43362], ["2011-09-09", 36442], ...
+  # => [["2011-09-19",  6516], ["2011-09-18", 43920], ["2011-09-17", 45412],
+  #     ["2011-09-16", 40972], ["2011-09-15", 37587], ["2011-09-14", 34918],
+  #     ["2011-09-13", 35223], ["2011-09-12", 37750], ["2011-09-11", 45057],
+  #     ["2011-09-10", 44077], ["2011-09-09", 36683], ["2011-09-08", 34871],
+  #     ["2011-09-07", 35960], ["2011-09-06", 35829], ["2011-09-05", 37777],
+  #     ["2011-09-04", 40233], ["2011-09-03", 39306], ["2011-09-02", 33467],
+  #     ["2011-09-01", 31558], ["2011-08-31", 32096], ["2011-08-30", 34076],
+  #     ["2011-08-29", 34950], ["2011-08-28", 40456], ["2011-08-27", 41332],
+  #     ["2011-08-26", 37737], ["2011-08-25", 34392], ["2011-08-24", 33560],
+  #     ["2011-08-23", 34722]]
   def metrics path, query={}, opts={}
     if weeks = opts.delete(:weeks)
       query[:startDate] =
@@ -59,23 +66,24 @@ module RestCore::Flurry::Client
   end
 
   # >> f.weekly(f.metrics('ActiveUsers', {}, :weeks => 4))
-  # => [258213, 247935, 260418, 236995]
+  # => [244548, 270227, 248513, 257149]
   def weekly array
-    start = Time.parse(array.last.first).to_i
+    start = Time.parse(array.first.first).to_i
     array.group_by{ |(date, value)|
       current = Time.parse(date).to_i
-      (current - start) / (86400*7)
+      - (current - start) / (86400*7)
     # calling .last to discard week numbers created by group_by
     }.sort.map(&:last).map{ |week|
       week.map{ |(date, num)| num }.inject(&:+) }
   end
 
   # >> f.sum(f.weekly(f.metrics('ActiveUsers', {}, :weeks => 4)))
-  # => [258213, 495870, 768771, 982343]
+  # => [1020437, 775889, 505662, 257149]
   def sum array
-    array.map.with_index{ |num, index|
-      num + array[1, index].inject(0, &:+)
-    }
+    reverse = array.reverse
+    reverse.map.with_index{ |_, index|
+      reverse[1, index].inject(reverse.first, &:+)
+    }.reverse
   end
 
   def query
