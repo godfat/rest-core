c1ad17ff6e614fac8ebafefc0f3460e56e09d062
Lin Jen-Shin <godfat@godfat.org>
2012-01-09 22:59:51 +0800

    let's try cool.io-http first

---
+++ lib/rest-core/app/coolio.rb
@@ -0,0 +1,20 @@
+
+require 'rest-core/middleware'
+
+require 'cool.io-http'
+
+class RestCore::Coolio
+  include RestCore::Middleware
+  def call env
+    Coolio::Http.request(:url     => request_uri(env),
+                         :payload => env[REQUEST_PAYLOAD],
+                         :headers => env[REQUEST_HEADERS]){ |response|
+
+      env[ASYNC].call(env.merge(RESPONSE_BODY    => response.body   ,
+                                RESPONSE_STATUS  => response.status ,
+                                RESPONSE_HEADERS => response.headers))
+    }
+
+    env
+  end
+end
