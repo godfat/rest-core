82d724964ba08e44e410ce2ff1da10baf114280b
Lin Jen-Shin <godfat@godfat.org>
2012-01-10 03:01:58 +0800

    dry.rb, rest-client.rb: respect ASYNC

--- lib/rest-core/app/dry.rb
+++ lib/rest-core/app/dry.rb
@@ -4,6 +4,7 @@ require 'rest-core/middleware'
 class RestCore::Dry
   include RestCore::Middleware
   def call env
+    env[ASYNC].call(env) if env[ASYNC]
     env
   end
 end
--- lib/rest-core/app/rest-client.rb
+++ lib/rest-core/app/rest-client.rb
@@ -6,19 +6,21 @@ require 'restclient'
 class RestCore::RestClient
   include RestCore::Middleware
   def call env
-    respond(env,
+    process(env,
             ::RestClient::Request.execute(:method  => env[REQUEST_METHOD ],
                                           :url     => request_uri(env)    ,
                                           :payload => env[REQUEST_PAYLOAD],
                                           :headers => env[REQUEST_HEADERS]))
 
   rescue ::RestClient::Exception => e
-    respond(env, e.response)
+    process(env, e.response)
   end
 
-  def respond env, response
-    env.merge(RESPONSE_BODY    => response.body,
-              RESPONSE_STATUS  => response.code,
-              RESPONSE_HEADERS => response.raw_headers)
+  def process env, response
+    result = env.merge(RESPONSE_BODY    => response.body,
+                       RESPONSE_STATUS  => response.code,
+                       RESPONSE_HEADERS => response.raw_headers)
+    result[ASYNC].call(result) if result[ASYNC]
+    result
   end
 end
