93c96caec72cfa02bf668ae7bee57629ddbdfc95
Lin Jen-Shin <godfat@godfat.org>
2012-02-18 02:26:26 +0800

    while ErrorDetectorHttp would always have a default

--- test/test_error_detector_http.rb
+++ test/test_error_detector_http.rb
@@ -3,10 +3,13 @@ require 'rest-core/test'
 
 describe RC::ErrorDetectorHttp do
   should 'lighten' do
-    RC::Builder.client do
+    client = RC::Builder.client do
       s = self.class # this is only for ruby 1.8!
       use s::ErrorDetectorHttp
       run s::Dry
-    end.new.lighten.attributes.should.key?(:error_detector)
+    end.new.lighten
+
+    client.attributes.should.key?(:error_detector)
+    client.error_detector.should.kind_of?(Proc)
   end
 end
