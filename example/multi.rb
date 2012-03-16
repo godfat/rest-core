edbc8d687e5ad07b5b911197eaef51aa3a489bf7
Lin Jen-Shin <godfat@godfat.org>
2012-03-17 03:07:59 +0800

    example/multi.rb: multi-requests example

---
+++ example/multi.rb
@@ -0,0 +1,33 @@
+
+require 'rest-core'
+require 'eventmachine'
+RestCore::EmHttpRequest # there might be a autoload bug?
+                        # omitting this line would cause
+                        # stack level too deep (SystemStackError)
+
+YourClient = RestCore::Builder.client do
+  s = self.class # this is only for ruby 1.8!
+  use s::DefaultSite , 'https://api.github.com/users/'
+  use s::JsonDecode  , true
+  use s::CommonLogger, method(:puts)
+  use s::Cache       , nil, 3600
+  run s::Auto
+end
+
+client = YourClient.new
+EM.run{
+  Fiber.new{
+    fiber = Fiber.current
+    result = {}
+    client.get('cardinalblue'){ |response|
+      result[0] = response
+      fiber.resume(result) if result.size == 2
+    }
+    client.get('cardinalblue'){ |response|
+      result[1] = response
+      fiber.resume(result) if result.size == 2
+    }
+    p Fiber.yield
+    EM.stop
+  }.resume
+}
