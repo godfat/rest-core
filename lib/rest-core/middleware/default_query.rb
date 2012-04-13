e60830a59e462787379c49fd3d98dbc7d6c7d23a
ayaya <ayaya@ayaya.tw>
2012-04-13 18:57:49 +0800

    make `use s::DefaultQuery` works fine

--- lib/rest-core/middleware/default_query.rb
+++ lib/rest-core/middleware/default_query.rb
@@ -4,6 +4,12 @@ require 'rest-core/middleware'
 class RestCore::DefaultQuery
   def self.members; [:query]; end
   include RestCore::Middleware
+
+  def initialize(*args)
+    super
+    @query ||= {}
+  end
+
   def call env
     app.call(env.merge(REQUEST_QUERY =>
       @query.merge(query(env)).merge(env[REQUEST_QUERY] || {})))
