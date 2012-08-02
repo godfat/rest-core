e795d8c3cbab33b045104352779b7f6c194ec27f
Lin Jen-Shin <godfat@godfat.org>
2012-08-02 23:37:03 +0800

    move before filter to top level

--- test/test_default_query.rb
+++ test/test_default_query.rb
@@ -2,43 +2,31 @@
 require 'rest-core/test'
 
 describe RC::DefaultQuery do
+  before do
+    @app = RC::DefaultQuery.new(RC::Dry.new, {})
+  end
+
   describe 'when given query' do
-    before do
-      @app = RC::DefaultQuery.new(RC::Dry.new, {})
-    end
-
-    def app
-      @app
-    end
-
     should 'do nothing' do
-      app.call({}){ |r| r[RC::REQUEST_QUERY].should.eq({}) }
+      @app.call({}){ |r| r[RC::REQUEST_QUERY].should.eq({}) }
     end
 
     should 'merge query' do
-      app.instance_eval{@query = {'q' => 'uery'}}
+      @app.instance_eval{@query = {'q' => 'uery'}}
 
-      app.call({}){ |r| r.should.eq({RC::REQUEST_QUERY => {'q' => 'uery'}}) }
+      @app.call({}){ |r| r.should.eq({RC::REQUEST_QUERY => {'q' => 'uery'}}) }
 
       format = {'format' => 'json'}
       env    = {RC::REQUEST_QUERY => format}
 
-      app.call(env){ |r|
+      @app.call(env){ |r|
         r.should.eq({RC::REQUEST_QUERY => {'q' => 'uery'}.merge(format)}) }
     end
   end
 
   describe 'when not given query' do
-    before do
-      @app = RC::DefaultQuery.new(RC::Dry.new)
-    end
-
-    def app
-      @app
-    end
-
     should 'merge query with {}' do
-      app.call({}){ |r| r.should.eq({RC::REQUEST_QUERY => {}}) }
+      @app.call({}){ |r| r.should.eq({RC::REQUEST_QUERY => {}}) }
     end
   end
 end
