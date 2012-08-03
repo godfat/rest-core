9cd2fe386f6d349e70068b62d7cea1d5016b3157
Lin Jen-Shin <godfat@godfat.org>
2012-08-03 15:14:27 +0800

    test_simple.rb: more tests

--- test/test_simple.rb
+++ test/test_simple.rb
@@ -2,10 +2,17 @@
 require 'rest-core/test'
 
 describe RC::Simple do
-  should 'work' do
-    path = 'http://example.com'
-    stub_request(:get, path).to_return(:body => 'OK')
-    EM.run{Fiber.new{RC::Simple.new.get(path).should.eq 'OK'; EM.stop}.resume}
+  before do
+    @path = 'http://example.com'
+    stub_request(:get, @path).to_return(:body => 'OK')
+  end
+
+  should 'work with EM' do
+    EM.run{Fiber.new{RC::Simple.new.get(@path).should.eq 'OK';EM.stop}.resume}
+  end
+
+  should 'work with RC' do
+    RC::Simple.new.get(@path).should.eq 'OK'
   end
 end
 
