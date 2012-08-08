e54a855520392aebb792d171440a238a846734bd
Lin Jen-Shin <godfat@godfat.org>
2012-08-08 21:37:24 +0800

    default_payload.rb: so that the default payload is {}

--- lib/rest-core/middleware/default_payload.rb
+++ lib/rest-core/middleware/default_payload.rb
@@ -4,6 +4,12 @@ require 'rest-core/middleware'
 class RestCore::DefaultPayload
   def self.members; [:payload]; end
   include RestCore::Middleware
+
+  def initialize *args
+    super
+    @payload ||= {}
+  end
+
   def call env, &k
     app.call(env.merge(REQUEST_PAYLOAD =>
       @payload.merge(payload(env)).merge(env[REQUEST_PAYLOAD] || {})), &k)
