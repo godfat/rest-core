f296fcf4fbbd2915713fe03a164bb6fcf280cf27
Lin Jen-Shin <godfat@godfat.org>
2012-08-09 01:06:51 +0800

    universal.rb: make it more clean

--- lib/rest-core/client/universal.rb
+++ lib/rest-core/client/universal.rb
@@ -1,19 +1,20 @@
 
-RestCore::Universal = RestCore::Builder.client do
-  s = RestCore
-  use s::Timeout       , 0
+module RestCore
+  Universal = Builder.client do
+    use Timeout       , 0
 
-  use s::DefaultSite   , nil
-  use s::DefaultHeaders, {}
-  use s::DefaultQuery  , {}
-  use s::DefaultPayload, {}
-  use s::AuthBasic     , nil, nil
+    use DefaultSite   , nil
+    use DefaultHeaders, {}
+    use DefaultQuery  , {}
+    use DefaultPayload, {}
+    use AuthBasic     , nil, nil
 
-  use s::FollowRedirect, 10
-  use s::CommonLogger  , method(:puts)
-  use s::Cache         ,  {}, 600 do
-    use s::ErrorHandler, nil
-    use s::ErrorDetectorHttp
-    use s::JsonResponse, false
+    use FollowRedirect, 10
+    use CommonLogger  , method(:puts)
+    use Cache         ,  {}, 600 do
+      use ErrorHandler, nil
+      use ErrorDetectorHttp
+      use JsonResponse, false
+    end
   end
 end
