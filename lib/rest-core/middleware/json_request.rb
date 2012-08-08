d588f720b5cbf4a8ad56c453bf877e249cfa669e
Lin Jen-Shin <godfat@godfat.org>
2012-08-09 01:04:04 +0800

    a simplified version of JsonRequest, only encode for payload if exists

---
+++ lib/rest-core/middleware/json_request.rb
@@ -0,0 +1,19 @@
+
+require 'rest-core/middleware'
+require 'rest-core/util/json'
+
+class RestCore::JsonRequest
+  def self.members; [:json_request]; end
+  include RestCore::Middleware
+
+  JSON_REQUEST_HEADER = {'Content-Type' => 'application/json'}.freeze
+
+  def call env, &k
+    return app.call(env, &k) unless json_request(env)
+    return app.call(env, &k) unless env[REQUEST_PAYLOAD]
+
+    app.call(env.merge(
+      REQUEST_HEADERS => JSON_REQUEST_HEADER.merge(env[REQUEST_HEADERS]),
+      REQUEST_PAYLOAD => Json.encode(env[REQUEST_PAYLOAD])              ), &k)
+  end
+end
---
+++ test/test_json_request.rb
@@ -0,0 +1,28 @@
+
+require 'rest-core/test'
+
+describe RC::JsonRequest do
+  app = RC::JsonRequest.new(RC::Dry.new, true)
+  env = {RC::REQUEST_HEADERS => {}}
+  request_params = {
+    'key' => 'value',
+    'array' => [1, 2, 3],
+    'nested' => {'k' => 'v', 'a' => [4, 5, 6]}
+  }
+
+  should 'encode payload as json' do
+    e = env.merge(RC::REQUEST_METHOD  => :post,
+                  RC::REQUEST_PAYLOAD => request_params)
+
+    app.call(e){ |res|
+      res.should.eq(
+        RC::REQUEST_METHOD  => :post,
+        RC::REQUEST_HEADERS => {'Content-Type' => 'application/json'},
+        RC::REQUEST_PAYLOAD => RC::Json.encode(request_params))}
+  end
+
+  should 'do nothing if json_request is false' do
+    app = RC::JsonRequest.new(RC::Dry.new, false)
+    app.call(env){ |res| res.should.eq res }
+  end
+end
