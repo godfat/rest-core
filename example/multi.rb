7d4472a85ec0099d8d8e628cb8bbaa829f4c401e
Lin Jen-Shin <godfat@godfat.org>
2012-08-09 23:11:55 +0800

    example/multi.rb: holy example

--- example/multi.rb
+++ example/multi.rb
@@ -12,9 +12,26 @@ end
 client = YourClient.new
 EM.run{
   Fiber.new{
-    p [client.get('cardinalblue'), client.get('godfat')]
+    p [client.get('cardinalblue')['name'], client.get('godfat')['name']]
     puts "But blocks here and do concurrent requests..."
     EM.stop
   }.resume
   puts "It's not blocking..."
 }
+
+puts; puts
+
+EM.run{
+  Thread.new{
+    p [client.get('cardinalblue')['name'], client.get('godfat')['name']]
+    puts "But blocks here and do concurrent requests..."
+    EM.stop
+  }
+  puts "Thread also works..."
+}
+
+puts; puts
+
+puts "Without EventMachine also works..."
+p [client.get('cardinalblue')['name'], client.get('godfat')['name']]
+puts "But blocks here and do concurrent requests..."
