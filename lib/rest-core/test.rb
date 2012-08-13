318594bdd53d70d0ca1618de7ab4a41f8a38aebb
Lin Jen-Shin <godfat@godfat.org>
2012-08-14 07:05:57 +0800

    test.rb: never used

--- lib/rest-core/test.rb
+++ lib/rest-core/test.rb
@@ -13,17 +13,6 @@ include WebMock::API
 WebMock.disable_net_connect!(:allow_localhost => true)
 Bacon.summary_on_exit
 
-module TestHelper
-  module_function
-  def normalize_query query
-    '?' + query[1..-1].split('&').sort.join('&')
-  end
-
-  def normalize_url url
-    url.sub(/\?.+/){ |query| TestHelper.normalize_query(query) }
-  end
-end
-
 module Kernel
   def eq? rhs
     self == rhs
