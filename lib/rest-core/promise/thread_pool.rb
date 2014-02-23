f7280d895f3fda8a21e5a6b1aff14230ab86bda8
Lin Jen-Shin <godfat@godfat.org>
2014-02-24 02:35:17 +0800

    spawn threads on demand
    TODO: timeout is not handled properly...

--- lib/rest-core/promise/thread_pool.rb
+++ lib/rest-core/promise/thread_pool.rb
@@ -16,51 +16,89 @@ class RestCore::Promise::ThreadPool
     def call
       @thread = Thread.current
       promise.synchronize{ job.call } unless cancelled
+      @thread = nil # should only kill the thread for user tasks
+      true
     rescue Exception => e # should never happen, but just in case
       warn "RestCore: ERROR: #{e}\n  from #{e.backtrace.inspect}"
+      promise.reject(e)   # should never deadlock someone
+      true
     end
     # called from the other thread telling us it's timed out
     def kill
       @cancelled = true
-      thread.kill if thread
-      pool.refresh
+      # we don't need to do anything if thread == Thread.current,
+      # because this thread would need to handle user callback,
+      # and the original request should already be cancelled and
+      # we're already sending back the exception
+      if thread && thread != Thread.current
+        # # don't kill for now, we're not yet properly handling this
+        # thread.kill
+      end
     end
     protected
-    attr_reader :thread, :cancelled
+    attr_reader :thread, :mutex, :cancelled
   end
 
   def self.[] client_class
     (@pools ||= {})[client_class] ||= new(client_class)
   end
 
-  attr_reader :client_class, :tasks, :workers
+  attr_reader :client_class
 
   def initialize client_class
-    @client_class  = client_class
-    @tasks         = Queue.new
-    @workers       = []
-    @workers_mutex = Mutex.new
-    refresh
+    @client_class = client_class
+    @queue        = Queue.new
+    @mutex        = Mutex.new
+    @workers      = []
+    @waiting      = 0
+    @spawned      = 0
   end
 
   def inspect
     "#<#{self.class.name} client_class=#{client_class}>"
   end
 
-  def defer promise, &job
-    refresh
-    tasks << task = Task.new(self, promise, job)
-    task
+  def max_size
+    client_class.pool_size
   end
 
-  def refresh
-    @workers_mutex.synchronize do
-      (client_class.pool_size - workers.size).times do
-        workers << Thread.new{
-          Thread.current.abort_on_exception = true
-          tasks.pop.call until @shutdown
-        }
-      end
+  def defer promise, &job
+    mutex.synchronize do
+      task = Task.new(self, promise, job)
+      queue << task
+      spawn_worker if waiting == 0 && spawned < max_size
+      task
     end
+  end
+
+  def trim force=false
+    queue << lambda{ false } if force || waiting > 0
+  end
+
+  def shutdown
+    max_size.times{ trim(true) }
+  end
+
+  protected
+  attr_reader :queue, :mutex, :condv, :workers, :waiting, :spawned
+
+  private
+  def spawn_worker
+    @spawned += 1
+    workers << Thread.new{
+      Thread.current.abort_on_exception = true # DEBUG ONLY
+
+      task = nil
+      begin
+        mutex.synchronize{ @waiting += 1 }
+        task = queue.pop
+        mutex.synchronize{ @waiting -= 1 }
+      end while task.call
+
+      mutex.synchronize do
+        workers.delete(thread)
+        @spawned -= 1
+      end
+    }
   end
 end
