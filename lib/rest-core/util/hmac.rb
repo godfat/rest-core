9716ab8f604d79e8bc844da079b122f130c77919
Lin Jen-Shin <godfat@godfat.org>
2014-04-29 19:03:56 +0800

    should also remove them

--- lib/rest-core/util/hmac.rb
+++ lib/rest-core/util/hmac.rb
@@ -4,8 +4,6 @@ require 'openssl'
 module RestCore; end
 module RestCore::Hmac
   module_function
-  # Fallback to ruby-hmac gem in case system openssl
-  # lib doesn't support SHA256 (OSX 10.5)
   def sha256 key, data
     OpenSSL::HMAC.digest('sha256', key, data)
   end
