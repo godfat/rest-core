7dae0562a670aeb8dacdd80a9e67cbe380bedbf4
Lin Jen-Shin <godfat@godfat.org>
2014-12-01 17:29:37 +0800

    i think we could just reuse that max_redirects...

--- lib/rest-core/middleware/follow_redirect.rb
+++ lib/rest-core/middleware/follow_redirect.rb
@@ -6,19 +6,15 @@ class RestCore::FollowRedirect
   include RestCore::Middleware
 
   def call env, &k
-    e = env.merge('follow_redirect.max_redirects' =>
-                    env['follow_redirect.max_redirects'] ||
-                    max_redirects(env))
-
-    if e[DRY]
-      app.call(e, &k)
+    if env[DRY]
+      app.call(env, &k)
     else
-      app.call(e){ |res| process(res, k) }
+      app.call(env){ |res| process(res, k) }
     end
   end
 
   def process res, k
-    return k.call(res) if res['follow_redirect.max_redirects'] <= 0
+    return k.call(res) if max_redirects(res) <= 0
     return k.call(res) if ![301,302,303,307].include?(res[RESPONSE_STATUS])
     return k.call(res) if  [301,302    ,307].include?(res[RESPONSE_STATUS]) &&
                           ![:get, :head    ].include?(res[REQUEST_METHOD])
@@ -33,7 +29,6 @@ class RestCore::FollowRedirect
     call(res.merge(REQUEST_METHOD => meth    ,
                    REQUEST_PATH   => location,
                    REQUEST_QUERY  => {}      ,
-                   'follow_redirect.max_redirects' =>
-                     res['follow_redirect.max_redirects'] - 1), &k)
+                   'max_redirects' => max_redirects(res) - 1), &k)
   end
 end
