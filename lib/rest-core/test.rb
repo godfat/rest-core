f4f63278e1edac739ac1da62ebcf90fe71207f9e
Lin Jen-Shin <godfat@godfat.org>
2016-02-04 17:16:41 +0800

    require it in its test

--- lib/rest-core/test.rb
+++ lib/rest-core/test.rb
@@ -5,8 +5,5 @@ require 'pork/auto'
 require 'muack'
 require 'webmock'
 
-# for testing lighten (serialization)
-require 'yaml'
-
 WebMock.disable_net_connect!(:allow_localhost => true)
 Pork::Executor.include(Muack::API, WebMock::API)
